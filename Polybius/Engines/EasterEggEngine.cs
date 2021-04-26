ed2a0fe41a7aa7426d23770ba7e52510e7d0d2da
Ernest Gu <ernest.gu.3.14@gmail.com>
2021-04-26 16:02:50 -0700

    Add an "easter egg" engine

---
+++ Polybius/Engines/EasterEggEngine.cs
@@ -0,0 +1,40 @@
+﻿using DSharpPlus.Entities;
+
+using System.Collections.Generic;
+using System.IO;
+
+namespace Polybius.Engines {
+	class EasterEggEngine : IEngine {
+		private const string path_db = @"db/easter_eggs.txt";
+		private const string delim = @"=";
+
+		public static List<SearchResult> search(Program.QueryMetaPair token) {
+			List<SearchResult> results = new ();
+
+			StreamReader db = new (path_db);
+			while (!db.EndOfStream) {
+				string line = db.ReadLine();
+				string[] split = line.Split(delim, 2);
+				string name = split[0], data = split[1];
+				if (name == token.query) {
+					results.Add(new EasterEggSearchResult {
+						is_exact_match = true,
+						similarity = 1.0F,
+						name = name,
+						data = data
+					});
+					break;
+				}
+			}
+			db.Close();
+
+			return results;
+		}
+
+		public class EasterEggSearchResult : SearchResult {
+			public override DiscordMessageBuilder get_display() {
+				return new DiscordMessageBuilder().WithContent(data);
+			}
+		}
+	}
+}
