277d26b4be12c180e8ef3dabf01fe3b8f582fd83
Ernest Gu <ernest.gu.3.14@gmail.com>
2021-04-26 16:00:05 -0700

    Basic search engine interface

---
+++ Polybius/Engines/IEngine.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Polybius.Engines {
+	interface IEngine {
+		public static List<SearchResult> search(Program.QueryMetaPair token) =>
+			throw new NotImplementedException();
+	}
+}
